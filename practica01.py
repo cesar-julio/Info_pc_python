c7fde1aa93c22fd1cfd3bed8a6e3cfbd21ab8875
cesar-julio <juliocesarlopezzelada@gmail.com>
2021-09-12 00:30:54 -0500

    script terminado
    Este es la vercion 1.0 del la app obtener información de una PC

--- practica01.py
+++ practica01.py
@@ -1,13 +1,18 @@
 import credenciales
+import main
 
 import smtplib, ssl
 
+msg = str(main.mostrarInfo())
 port = 465  # For SSL
 smtp_server = "smtp.gmail.com"
 sender_email = credenciales.email # Enter your address
 receiver_email = credenciales.email # Enter receiver address
 password = credenciales.passw
-message = "Este es un mesaje enviado desde python para probar que si funciona este servicio"
+message = f"""\
+Subject: Hi there
+
+This message is sent from Python.{msg}"""
 
 context = ssl.create_default_context()
 with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
